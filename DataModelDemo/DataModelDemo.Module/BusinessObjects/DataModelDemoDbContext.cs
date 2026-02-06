5fbbc5203d91b88ee17f13dcef752ef3b0befeee
SamirNakrani <SamirNakrani@SAMIRNA-ANDAZE>
2026-02-06 14:09:55 +0530

    Add project files.

---
+++ DataModelDemo/DataModelDemo.Module/BusinessObjects/DataModelDemoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.ExpressApp.EFCore.Updating;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DataModelDemo.Module.BusinessObjects
+{
+    [TypesInfoInitializer(typeof(DbContextTypesInfoInitializer<DataModelDemoEFCoreDbContext>))]
+    public class DataModelDemoEFCoreDbContext : DbContext
+    {
+        public DataModelDemoEFCoreDbContext(DbContextOptions<DataModelDemoEFCoreDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.UseDeferredDeletion(this);
+            modelBuilder.UseOptimisticLock();
+            modelBuilder.SetOneToManyAssociationDeleteBehavior(DeleteBehavior.SetNull, DeleteBehavior.Cascade);
+            modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+            modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+        }
+    }
+}
