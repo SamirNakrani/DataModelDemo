34f53848a43f688686fce4fd089de1d671cb29be
SamirNakrani <samirna@andaze.com>
2026-02-06 16:00:58 +0530

    Implemented Data Models For Employee And Department and Add Relationship one To Many

---
+++ DataModelDemo/DataModelDemo.Module/BusinessObjects/Employee.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModelDemo.Module.BusinessObjects
+{
+    [ObjectCaptionFormat("{0:FullName}")]
+    [DefaultProperty(nameof(FullName))]
+    [DefaultClassOptions]
+    public class Employee: BaseObject
+    {
+        public virtual String FirstName { get; set; }
+        public virtual String LastName { get; set; }
+        public virtual String MiddleName { get; set; }
+
+        public virtual DateTime? Birthday { get; set; }
+
+        //Use this attribute to hide or show the editor of this property in the UI.
+        [Browsable(false)]
+        public virtual int TitleOfCourtesy_Int { get; set; }
+
+        //Use this attribute to exclude the property from database mapping.
+        [NotMapped]
+        public virtual TitleOfCourtesy TitleOfCourtesy { get; set; }
+
+        [SearchMemberOptions(SearchMemberMode.Exclude)]
+        public String FullName
+        {
+            get { return ObjectFormatter.Format(FullNameFormat, this, EmptyEntriesMode.RemoveDelimiterWhenEntryIsEmpty); }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public String DisplayName
+        {
+            get { return FullName; }
+        }
+
+        public static String FullNameFormat = "{FirstName} {MiddleName} {LastName}";
+
+        [FieldSize(255)]
+        public virtual String Email { get; set; }
+
+        [RuleRegularExpression(@"(((http|https)\://)[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;amp;%\$#\=~])*)|([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})", CustomMessageTemplate = @"Invalid ""Web Page Address"".")]
+        public virtual string WebPageAddress { get; set; }
+
+        [StringLength(4096)]
+        public virtual string Notes { get; set; }
+        public virtual Department Department { get; set; }
+    }
+    public enum TitleOfCourtesy
+    {
+        Dr,
+        Miss,
+        Mr,
+        Mrs,
+        Ms
+    }
+}
