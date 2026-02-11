1ab7005b9fb009dc3d29806bd466739200237200
SamirNakrani <samirna@andaze.com>
2026-02-11 17:53:12 +0530

    Add custom Blazor string property editor and model updates

---
+++ DataModelDemo/DataModelDemo.Blazor.Server/Editors/ComponentModelBase.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+
+namespace DataModelDemo.Blazor.Server.Editors
+{
+    public class InputTextModel : ComponentModelBase
+    {
+        public string Value
+        {
+            get => GetPropertyValue<string>();
+            set => SetPropertyValue(value);
+        }
+        public EventCallback<string> ValueChanged
+        {
+            get => GetPropertyValue<EventCallback<string>>();
+            set => SetPropertyValue(value);
+        }
+        public Expression<Func<string>> ValueExpression
+        {
+            get => GetPropertyValue<Expression<Func<string>>>();
+            set => SetPropertyValue(value);
+        }
+        public override Type ComponentType => typeof(InputText);
+    }
+}
